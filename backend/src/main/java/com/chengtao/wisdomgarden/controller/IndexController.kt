274271ec45b34dc729af5013a8b4d6ce29aee6e5
ChengTao <chengtaolearn@163.com>
2017-09-28 20:08:53 +0800

    refactor:代码规范

--- backend/src/main/java/com/chengtao/wisdomgarden/controller/IndexController.kt
+++ backend/src/main/java/com/chengtao/wisdomgarden/controller/IndexController.kt
@@ -10,8 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping
 @Controller
 class IndexController {
     @RequestMapping("/")
-    fun index(@CookieValue(value = WisdomGardenConfig.Cookies.USER_NAME, defaultValue = "") userName: String,
-              @CookieValue(value = WisdomGardenConfig.Cookies.PASSWORD, defaultValue = "") password: String,
+    fun index(@CookieValue(value = WisdomGardenConfig.Cookies.USER_NAME, defaultValue = "")
+              userName: String,
+              @CookieValue(value = WisdomGardenConfig.Cookies.PASSWORD, defaultValue = "")
+              password: String,
               model: Model): String {
         if (StringUtils.isStringNull(userName, password)) {
             model.addAttribute(WisdomGardenConfig.TITLE, WisdomGardenConfig.View.Login.TITLE)
--- backend/src/main/java/com/chengtao/wisdomgarden/utils/StringUtils.kt
+++ backend/src/main/java/com/chengtao/wisdomgarden/utils/StringUtils.kt
@@ -1,12 +1,7 @@
 class StringUtils {
     companion object {
         fun isStringNull(vararg stings: String): Boolean {
-            for (s: String in stings) {
-                if (s == null || s.length == 0) {
-                    return true
-                }
-            }
-            return false
+            return stings.any { it == null || it.length == 0 }
         }
     }
 }
