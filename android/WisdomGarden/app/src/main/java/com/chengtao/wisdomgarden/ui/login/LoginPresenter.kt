dbd03cb732ade300ca28d2e13d97fe3598330393
ChengTao <chengtaolearn@163.com>
2017-10-19 16:29:34 +0800

    refactor:完成登录界面逻辑

--- android/WisdomGarden/app/src/main/java/com/chengtao/wisdomgarden/ui/login/LoginPresenter.kt
+++ android/WisdomGarden/app/src/main/java/com/chengtao/wisdomgarden/ui/login/LoginPresenter.kt
@@ -1,6 +1,8 @@
 package com.chengtao.wisdomgarden.ui.login
 
 import com.chengtao.wisdomgarden.WisdomGardenPresenter
+import com.chengtao.wisdomgarden.request.LoginRequest
+import com.chengtao.wisdomgarden.utils.MD5Util
 
 /**
  * Author : ChengTao(chengtaolearn@163.com)
@@ -11,11 +13,36 @@ import com.chengtao.wisdomgarden.WisdomGardenPresenter
 class LoginPresenter(
     loginView: LoginContract.LoginView) : WisdomGardenPresenter<LoginContract.LoginView>(
     loginView), LoginContract.LoginPresenter {
+  companion object {
+    val LOGIN_REQUEST: Short = 1
+  }
+
+  private val loginRequest = LoginRequest(this)
+
   override fun login(userName: String, password: String) {
-    println("userName:$userName-->password:$password")
+    when {
+      userName == "" -> {
+        mView?.toast("用户名不能为空")
+      }
+      password == "" -> {
+        mView?.to("密码不能为空")
+      }
+      else -> {
+        loginRequest.userName = userName
+        loginRequest.password = MD5Util.md5(password)
+        loginRequest.requestId = LOGIN_REQUEST
+        loginRequest.execute()
+      }
+    }
   }
 
   override fun onData(requestId: Short, response: Any?) {
+    when (requestId) {
+      LOGIN_REQUEST -> {
+        if (response != null) {
+          TODO("保存用户信息,登录成功,跳转主界面")
+        }
+      }
+    }
   }
-
 }
